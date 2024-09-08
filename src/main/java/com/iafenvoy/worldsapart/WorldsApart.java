69bc46521f448594f02e6dc3a56aeda84055c993
IAFEnvoy <tanksheng545@126.com>
2024-09-08 14:25:37 +0800

    update active mode of sword

--- src/main/java/com/iafenvoy/worldsapart/WorldsApart.java
+++ src/main/java/com/iafenvoy/worldsapart/WorldsApart.java
@@ -4,6 +4,7 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -18,14 +19,17 @@ public class WorldsApart {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MOD_ID);
 
     public static final RegistryObject<Item> KRISTA_SWORD = ITEMS.register("krista_sword", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> KRISTA_SWORD_ACTIVE = ITEMS.register("krista_sword_active", () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> VAREK_SWORD = ITEMS.register("varek_sword", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> VAREK_SWORD_ACTIVE = ITEMS.register("varek_sword_active", () -> new Item(new Item.Properties()));
 
     public static final RegistryObject<CreativeModeTab> MAIN = CREATIVE_MODE_TABS.register("main", () -> CreativeModeTab.builder()
-            .withTabsBefore(CreativeModeTabs.COMBAT)
-            .icon(() -> KRISTA_SWORD.get().getDefaultInstance())
+            .icon(() -> new ItemStack(KRISTA_SWORD.get()))
             .displayItems((parameters, output) -> {
                 output.accept(KRISTA_SWORD.get());
+                output.accept(KRISTA_SWORD_ACTIVE.get());
                 output.accept(VAREK_SWORD.get());
+                output.accept(VAREK_SWORD_ACTIVE.get());
             }).build());
 
     public WorldsApart() {
